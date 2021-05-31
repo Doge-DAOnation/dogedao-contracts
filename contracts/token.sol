e5716f78296785c1a71ff3ed92f69c16051e5e73
Quintin <quintinmakwe@gmail.com>
2021-05-31 09:32:36 +0100

    send to pool functions

--- contracts/token.sol
+++ contracts/token.sol
@@ -3,7 +3,26 @@ pragma solidity ^0.8.4;
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract DogFundMeToken is ERC20{
+    address _owner;
+    uint _initialSupply;
+    bool poolFunded = false;
+    
+    modifier onlyOwner(){
+        require(msg.sender == _owner);
+        _;
+    }
+
     constructor(uint256 initialSupply) ERC20('DogFundMe', 'DFM'){
-        _mint(msg.sender, initialSupply);
+        _owner = msg.sender;
+        _initialSupply = initialSupply;
+        
+        _mint(_owner, initialSupply);
+    }
+    
+    function sendToPool(address bPAA, address uSPA, uint bPAPercentage, uint uSPAPercentage) external onlyOwner{
+        require(!poolFunded && (bPAPercentage + uSPAPercentage) == 100);
+        _transfer(_owner, bPAA, ((bPAPercentage/100) * _initialSupply));
+        _transfer(_owner, uSPA, ((uSPAPercentage/100) * _initialSupply));
+        poolFunded = true;
     }
 }
