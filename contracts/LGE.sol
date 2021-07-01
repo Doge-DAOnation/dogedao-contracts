336aeacf3498a8616a60ed1623ab67dd9cc9960c
Quintin <quintinmakwe@gmail.com>
2021-07-02 00:31:21 +0100

    updated truffle config

--- contracts/LGE.sol
+++ contracts/LGE.sol
@@ -1,156 +1,164 @@
 pragma solidity ^0.8.4;
 
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/PausableToken.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/TokenTimelock.sol";
-import "openzeppelin-solidity/contracts/crowdsale/Crowdsale.sol";
-import "openzeppelin-solidity/contracts/crowdsale/emission/MintedCrowdsale.sol";
-import "openzeppelin-solidity/contracts/crowdsale/validation/CappedCrowdsale.sol";
-import "openzeppelin-solidity/contracts/crowdsale/validation/TimedCrowdsale.sol";
-import "openzeppelin-solidity/contracts/crowdsale/validation/WhitelistedCrowdsale.sol";
-import "openzeppelin-solidity/contracts/crowdsale/distribution/RefundableCrowdsale.sol";
+contract LGEContract{
+    
+    event HarvestRequestApproval(uint request_id);
 
-contract DappTokenCrowdsale is Crowdsale, MintedCrowdsale, CappedCrowdsale, TimedCrowdsale, WhitelistedCrowdsale, RefundableCrowdsale {
+    event LiquidityCreated(address provider_address);
+    
+    address public _owner;
+    
+    mapping (address => bool) private harvesters;
+    
+    address[] harvesters_arr;
+    
+    bool public harvestersAdded = false;
+    
+    address[] liquidityProviders_arr;
+    
+    uint _min_confirmation;
+    
+    mapping (address => uint) public liquidityProviders;
+    
+    mapping (uint => harvestRequest) public harvestRequests;
+    
+    //this maps the harvest request id to addresses that have confirmed request. Neccessary to ensure no address verifies a request more than once
+    mapping(uint => mapping(address => bool)) confirmers;
+    
+   harvestRequest[] harvest_rqst_arr;
+   
+    struct harvestRequest{
+        address initiator;
+        address payable withdraw_addres;
+        uint value;
+        uint no_of_confirmations;
+        bool executed;
+        bool active;
+        string harvest_purpose;
+    }
+    
+    enum contractStateEnum {
+        active,
+        paused
+    }
+    
+    contractStateEnum public _cState = contractStateEnum.active;
+    
 
-  // Track investor contributions
-  uint256 public investorMinCap = 2000000000000000; // 0.002 ether
-  uint256 public investorHardCap = 50000000000000000000; // 50 ether
-  mapping(address => uint256) public contributions;
-
-  // Crowdsale Stages
-  enum CrowdsaleStage { PreICO, ICO }
-  // Default to presale stage
-  CrowdsaleStage public stage = CrowdsaleStage.PreICO;
-
-  // Token Distribution
-  uint256 public tokenSalePercentage   = 70;
-  uint256 public foundersPercentage    = 10;
-  uint256 public foundationPercentage  = 10;
-  uint256 public partnersPercentage    = 10;
-
-  // Token reserve funds
-  address public foundersFund;
-  address public foundationFund;
-  address public partnersFund;
-
-  // Token time lock
-  uint256 public releaseTime;
-  address public foundersTimelock;
-  address public foundationTimelock;
-  address public partnersTimelock;
-
-  constructor(
-    uint256 _rate,
-    address _wallet,
-    ERC20 _token,
-    uint256 _cap,
-    uint256 _openingTime,
-    uint256 _closingTime,
-    uint256 _goal,
-    address _foundersFund,
-    address _foundationFund,
-    address _partnersFund,
-    uint256 _releaseTime
-  )
-    Crowdsale(_rate, _wallet, _token)
-    CappedCrowdsale(_cap)
-    TimedCrowdsale(_openingTime, _closingTime)
-    RefundableCrowdsale(_goal)
-    public
-  {
-    require(_goal <= _cap);
-    foundersFund   = _foundersFund;
-    foundationFund = _foundationFund;
-    partnersFund   = _partnersFund;
-    releaseTime    = _releaseTime;
-  }
-
-  /**
-  * @dev Returns the amount contributed so far by a sepecific user.
-  * @param _beneficiary Address of contributor
-  * @return User contribution so far
-  */
-  function getUserContribution(address _beneficiary)
-    public view returns (uint256)
-  {
-    return contributions[_beneficiary];
-  }
-
-  /**
-  * @dev Allows admin to update the crowdsale stage
-  * @param _stage Crowdsale stage
-  */
-  function setCrowdsaleStage(uint _stage) public onlyOwner {
-    if(uint(CrowdsaleStage.PreICO) == _stage) {
-      stage = CrowdsaleStage.PreICO;
-    } else if (uint(CrowdsaleStage.ICO) == _stage) {
-      stage = CrowdsaleStage.ICO;
+    modifier onlyOwner(){
+        require(msg.sender == _owner, 'Not an owner');
+        _;
+    }
+    
+    modifier isActiveHarvestRequest(uint harvest_id){
+        require(harvestRequests[harvest_id].active, "Supplied harvest request is currently not active");
+        _;
+    }
+    
+    modifier onlyHarvester(){
+        require(harvesters[msg.sender], 'Invalid Harvester');
+        _;
     }
 
-    if(stage == CrowdsaleStage.PreICO) {
-      rate = 500;
-    } else if (stage == CrowdsaleStage.ICO) {
-      rate = 250;
+    constructor (uint min_confirmation){
+        _owner = msg.sender;
+        harvesters[msg.sender] = true;
+        harvesters_arr.push(msg.sender);
+        _min_confirmation = min_confirmation;
     }
-  }
-
-  /**
-   * @dev forwards funds to the wallet during the PreICO stage, then the refund vault during ICO stage
-   */
-  function _forwardFunds() internal {
-    if(stage == CrowdsaleStage.PreICO) {
-      wallet.transfer(msg.value);
-    } else if (stage == CrowdsaleStage.ICO) {
-      super._forwardFunds();
+    
+    function addHarvesters(address[] memory founders) public onlyOwner {
+        require(!harvestersAdded, "Harvesters already supplied");
+        require(founders.length <= 4, "Exceeded total number of required harvesters");
+        uint i;
+        for(i = 0; i < founders.length; i++){
+            require(!harvesters[founders[i]], 'Founder is already added');
+            harvesters[founders[i]] = true;
+            harvesters_arr.push(founders[i]);
+        }
+        harvestersAdded = true;
     }
-  }
-
-  /**
-  * @dev Extend parent behavior requiring purchase to respect investor min/max funding cap.
-  * @param _beneficiary Token purchaser
-  * @param _weiAmount Amount of wei contributed
-  */
-  function _preValidatePurchase(
-    address _beneficiary,
-    uint256 _weiAmount
-  )
-    internal
-  {
-    super._preValidatePurchase(_beneficiary, _weiAmount);
-    uint256 _existingContribution = contributions[_beneficiary];
-    uint256 _newContribution = _existingContribution.add(_weiAmount);
-    require(_newContribution >= investorMinCap && _newContribution <= investorHardCap);
-    contributions[_beneficiary] = _newContribution;
-  }
-
-
-  /**
-   * @dev enables token transfers, called when owner calls finalize()
-  */
-  function finalization() internal {
-    if(goalReached()) {
-      MintableToken _mintableToken = MintableToken(token);
-      uint256 _alreadyMinted = _mintableToken.totalSupply();
-
-      uint256 _finalTotalSupply = _alreadyMinted.div(tokenSalePercentage).mul(100);
-
-      foundersTimelock   = new TokenTimelock(token, foundersFund, releaseTime);
-      foundationTimelock = new TokenTimelock(token, foundationFund, releaseTime);
-      partnersTimelock   = new TokenTimelock(token, partnersFund, releaseTime);
-
-      _mintableToken.mint(address(foundersTimelock),   _finalTotalSupply.mul(foundersPercentage).div(100));
-      _mintableToken.mint(address(foundationTimelock), _finalTotalSupply.mul(foundationPercentage).div(100));
-      _mintableToken.mint(address(partnersTimelock),   _finalTotalSupply.mul(partnersPercentage).div(100));
-
-      _mintableToken.finishMinting();
-      // Unpause the token
-      PausableToken _pausableToken = PausableToken(token);
-      _pausableToken.unpause();
-      _pausableToken.transferOwnership(wallet);
+    
+    function getContractBalance() public view onlyHarvester returns(uint){
+        return address(this).balance;
     }
+    
+    function addLiquidity() payable public{
+        require(msg.value > 0 wei, "Please provide minimum amount of 1 wei");
+        require(_cState == contractStateEnum.active, "Not currently accepting liquidity, check back in a future time");
+        if(liquidityProviders[msg.sender] == 0){
+            liquidityProviders_arr.push(msg.sender);
+        }
+        liquidityProviders[msg.sender] += msg.value;
 
-    super.finalization();
-  }
-
+        //emit event 
+        emit LiquidityCreated(msg.sender);
+        return;
+    }
+    
+    function getLiquidityProviders() public view returns(address[] memory){
+        return liquidityProviders_arr;
+    }
+    
+    function updateContractState(uint _status) public onlyHarvester{
+        require(_status <= 1);
+        _cState = contractStateEnum(_status);
+        return;
+    }
+    
+    function getHarvesters() external view returns(address[] memory){
+        return harvesters_arr;
+    }
+    
+    function createHarvestRequest(uint _value, address payable _withdraw_address, string memory _harvest_purpose ) public onlyHarvester{
+        require(_value < address(this).balance, "Request amount above contract balance");
+        require(_value > 0 wei, "Value of request should be above 0 wei");
+        harvestRequests[harvest_rqst_arr.length] = harvestRequest({
+            initiator: msg.sender,
+            value: _value,
+            no_of_confirmations: 0,
+            executed: false,
+            active: true,
+            withdraw_addres: _withdraw_address,
+            harvest_purpose: _harvest_purpose
+        });
+        
+        harvest_rqst_arr.push(harvestRequests[harvest_rqst_arr.length]);
+    }
+    
+    function approveHarvestRequest(uint harvest_id) public onlyHarvester isActiveHarvestRequest(harvest_id){
+        //check that harvest id is within range
+        require(harvest_id <= harvest_rqst_arr.length, "No such request");
+        //verify that msg.sender doesn't have a prior approval 
+        require(!confirmers[harvest_id][msg.sender], "Already confirmed request");
+        //verify that harvest request is active
+        require(harvestRequests[harvest_id].active, "Harvest request is currently not active");
+        harvestRequests[harvest_id].no_of_confirmations += 1;
+        
+        //update the confirmers 
+        confirmers[harvest_id][msg.sender] = true;
+        
+        //if the request has exceeded the applied threshhold, emit event
+        if(harvestRequests[harvest_id].no_of_confirmations >= _min_confirmation){
+            emit HarvestRequestApproval(harvest_id);
+        }
+    }
+    
+    function resetHarvestRequestStatus(uint harvest_id) public onlyHarvester{
+        require(harvestRequests[harvest_id].initiator == msg.sender, "You can only update request created by you");
+        harvestRequests[harvest_id].active = !harvestRequests[harvest_id].active;
+    }
+    
+    function getHarvestRequest()public view onlyHarvester returns(harvestRequest[] memory){
+        return harvest_rqst_arr;
+    }
+    
+    function harvestLiquidity(uint harvest_request_id)public onlyHarvester isActiveHarvestRequest(harvest_request_id){
+        require(!harvestRequests[harvest_request_id].executed, "Request already harvested");
+        require(harvestRequests[harvest_request_id].no_of_confirmations >= _min_confirmation, "Harvest request confirmation below required minimum");
+        require(harvestRequests[harvest_request_id].initiator == msg.sender, "Can only harvest self created requests");
+        uint value = harvestRequests[harvest_request_id].value;
+        harvestRequests[harvest_request_id].withdraw_addres.transfer(value);
+        harvestRequests[harvest_request_id].executed = true;
+    }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,6 @@
 const LGEContract = artifacts.require("LGEContract");
 
 module.exports = function (deployer, network, accounts) {
-    const numConfirmationsRequired = 2; 
+    const numConfirmationsRequired = 3; 
     deployer.deploy(LGEContract, numConfirmationsRequired);
 };
